735e18bbb52c6bb76d4b74598670c8818f87d446
nieznany.sprawiciel <nieznany.sprawiciel@gmail.com>
2022-08-30 16:28:45 +0200

    Test forwarding to node alias when Nodes are private

--- server/tests/test_discovery.rs
+++ server/tests/test_discovery.rs
@@ -1,3 +1,5 @@
+mod helpers;
+
 use anyhow::Context;
 use futures::SinkExt;
 use std::rc::Rc;
@@ -10,6 +12,8 @@ use ya_relay_client::ClientBuilder;
 use ya_relay_core::crypto::{CryptoProvider, FallbackCryptoProvider};
 use ya_relay_core::key::generate;
 use ya_relay_server::testing::server::init_test_server;
+
+use helpers::hack_make_ip_private;
 
 #[serial_test::serial]
 async fn test_find_node_by_alias() -> anyhow::Result<()> {
@@ -59,5 +63,59 @@ async fn test_find_node_by_alias() -> anyhow::Result<()> {
 
     assert!(received1.load(SeqCst));
     assert!(received2.load(SeqCst));
+    Ok(())
+}
+
+#[serial_test::serial]
+async fn test_find_node_by_alias_private_ip() -> anyhow::Result<()> {
+    let wrapper = init_test_server().await?;
+
+    let mut crypto = FallbackCryptoProvider::default();
+    crypto.add(generate());
+
+    let alias = crypto.aliases().await.unwrap()[0];
+
+    let client1 = ClientBuilder::from_url(wrapper.url())
+        .connect()
+        .build()
+        .await?;
+    let client2 = ClientBuilder::from_url(wrapper.url())
+        .connect()
+        .crypto(crypto)
+        .build()
+        .await?;
+
+    hack_make_ip_private(&wrapper, &client1).await;
+    hack_make_ip_private(&wrapper, &client2).await;
+
+    let _rx1 = client1
+        .forward_receiver()
+        .await
+        .context("no forward receiver")?;
+    let rx2 = client2
+        .forward_receiver()
+        .await
+        .context("no forward receiver")?;
+
+    let received2 = Rc::new(AtomicBool::new(false));
+
+    println!("Setting up");
+
+    spawn_receive(">> 2", received2.clone(), rx2);
+
+    println!("Forwarding: unreliable");
+
+    let mut tx1 = client1.forward_unreliable(alias).await.unwrap();
+    tx1.send(vec![1u8]).await?;
+
+    tokio::time::sleep(Duration::from_millis(100)).await;
+    assert!(received2.load(SeqCst));
+
+    let mut tx1 = client1.forward_unreliable(client2.node_id()).await.unwrap();
+    tx1.send(vec![1u8]).await?;
+
+    tokio::time::sleep(Duration::from_millis(100)).await;
+    assert!(received2.load(SeqCst));
+
     Ok(())
 }
