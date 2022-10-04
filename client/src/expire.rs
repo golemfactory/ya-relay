994101d21f58fa6f0c152b279786cc5854fa438a
Wojciech Kargul <wojciech.kargul@gmail.com>
2022-10-04 15:53:27 +0200

    formatting fix

--- client/src/expire.rs
+++ client/src/expire.rs
@@ -56,7 +56,7 @@ async fn close_sessions(layer: SessionManager, sessions: Vec<Arc<Session>>, expi
 
         if session.remote == layer.config.srv_addr {
             let _ = layer.drop_server_session().await;
-            continue
+            continue;
         }
 
         layer
