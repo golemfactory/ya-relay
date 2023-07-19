76605c935cef8b09de02e273b2652a7e9dde5bd2
Rafał Borysionek <22373844+MrDarthShoe@users.noreply.github.com>
2023-07-19 13:46:15 +0200

    Fix perf building

--- client/examples/perf.rs
+++ client/examples/perf.rs
@@ -586,6 +586,7 @@ mod relaying {
     use std::sync::Arc;
     use tokio_stream::wrappers::UnboundedReceiverStream;
     use ya_relay_client::proto::Payload;
+    use ya_relay_client::GenericSender;
     use ya_relay_core::NodeId;
 
     #[derive(Clone, Debug, Default, Serialize)]
@@ -848,7 +849,8 @@ mod relaying {
             .collect::<Result<Vec<Scenario>, _>>()?;
 
         log::info!("Running idle discovery calls.");
-        let idle = tokio::task::spawn_local(async move { future::join_all(futures.into_iter()).await });
+        let idle =
+            tokio::task::spawn_local(async move { future::join_all(futures.into_iter()).await });
 
         log::info!("Running {} scenario(s) (Requestor(s))", scenarios.len());
 
@@ -926,8 +928,9 @@ mod relaying {
             })
             .collect::<Vec<_>>();
 
-        let providers_handle =
-            tokio::task::spawn_local(async move { future::join_all(provider_futures.into_iter()).await });
+        let providers_handle = tokio::task::spawn_local(async move {
+            future::join_all(provider_futures.into_iter()).await
+        });
 
         // TODO: Should be asynchronous, we can't wait for operation finish.
         let start = tokio::time::Instant::now();
@@ -939,7 +942,7 @@ mod relaying {
 
             let data = (0..action.size).map(|n| n as u8).collect();
 
-            let mut sender = requestor.forward(action.remote_id).await?;
+            let mut sender = requestor.forward_reliable(action.remote_id).await?;
             sender.send(Payload::Vec(data)).await.ok();
         }
 
