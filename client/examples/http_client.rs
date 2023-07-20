74ed90546cb2d325a728f57629ec34b11b7cb1e3
Rafał Borysionek <22373844+MrDarthShoe@users.noreply.github.com>
2023-07-20 13:12:20 +0200

    Using actix_web

--- client/examples/http_client.rs
+++ client/examples/http_client.rs
@@ -1,3 +1,6 @@
+use std::{convert::TryFrom, str::FromStr};
+
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
 use anyhow::Result;
 use structopt::StructOpt;
 
@@ -5,6 +8,7 @@ use ya_relay_client::{Client, ClientBuilder, FailFast};
 use ya_relay_core::{
     crypto::FallbackCryptoProvider,
     key::{load_or_generate, Protected},
+    NodeId,
 };
 
 #[derive(StructOpt)]
@@ -19,11 +23,33 @@ struct Cli {
     password: Option<Protected>,
 }
 
+#[derive(Clone)]
+struct AppState {
+    client: std::sync::Arc<std::sync::Mutex<Client>>,
+}
+
+#[get("/find-node/{node_id}")]
+async fn find_node(node_id: web::Path<String>) -> impl Responder {
+    let node_id = node_id.parse::<NodeId>().unwrap();
+
+    let msg = format!("Node id: {node_id} found in s\n");
+
+    HttpResponse::Ok().body(msg)
+}
 #[tokio::main(flavor = "current_thread")]
+// #[actix_web::main]
 async fn main() -> Result<()> {
+    env_logger::init();
+
     let cli = Cli::from_args();
 
     let client = build_client(cli.relay_addr, &cli.key_file, cli.password).await?;
+
+    let app_data = std::sync::Arc::new(std::sync::Mutex::new(client));
+
+    let server = HttpServer::new(|| App::new().app_data(app_data).service(find_node));
+
+    server.bind(("0.0.0.0", cli.port))?.run().await?;
 
     Ok(())
 }
@@ -36,5 +62,11 @@ async fn build_client(
     let secret = load_or_generate(&key_file, password);
     let builder = ClientBuilder::from_url(relay_addr).crypto(FallbackCryptoProvider::new(secret));
 
-    builder.connect(FailFast::Yes).build().await
+    let client = builder.connect(FailFast::Yes).build().await?;
+
+    log::info!("CLIENT NODE ID: {}", client.node_id());
+    log::info!("CLIENT BIND ADDR: {:?}", client.bind_addr().await);
+    log::info!("CLIENT PUBLIC ADDR: {:?}", client.public_addr().await);
+
+    Ok(client)
 }
