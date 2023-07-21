9ddb04ce9641c21fd31b6414445632818e90a3b4
Rafał Borysionek <22373844+MrDarthShoe@users.noreply.github.com>
2023-07-21 14:10:08 +0200

    WIP pings

--- client/examples/http_client.rs
+++ client/examples/http_client.rs
@@ -1,11 +1,15 @@
-use ya_relay_client::{Client, ClientBuilder, FailFast};
+use ya_relay_client::{Client, ClientBuilder, FailFast, ForwardReceiver, GenericSender};
 use ya_relay_core::{
     crypto::FallbackCryptoProvider,
     key::{load_or_generate, Protected},
     NodeId,
 };
 
-use std::time::{Duration, Instant};
+use std::{
+    collections::HashMap,
+    sync::{Arc, Mutex},
+    time::{Duration, Instant},
+};
 
 use actix_web::{
     get,
@@ -31,13 +35,22 @@ struct Cli {
     password: Option<Protected>,
 }
 
+type Pings = Arc<Mutex<HashMap<u32, oneshot::Sender<Duration>>>>;
+
 #[derive(Debug)]
 enum Command {
     FindNode(FindNode),
+    Ping(Ping),
 }
 
 #[derive(Debug)]
 struct FindNode {
+    node_id: NodeId,
+    response: oneshot::Sender<Duration>,
+}
+
+#[derive(Debug)]
+struct Ping {
     node_id: NodeId,
     response: oneshot::Sender<Duration>,
 }
@@ -67,7 +80,59 @@ async fn find_node(
     ))
 }
 
-async fn client_handler(client: Client, mut rx: mpsc::Receiver<Command>) -> Result<()> {
+#[get("/ping/{node_id}")]
+async fn ping(
+    node_id: web::Path<String>,
+    client_sender: web::Data<Sender<Command>>,
+) -> impl Responder {
+    let node_id = node_id.parse::<NodeId>().unwrap();
+
+    let (rx, tx) = oneshot::channel::<Duration>();
+
+    client_sender
+        .send(Command::Ping(Ping {
+            node_id,
+            response: rx,
+        }))
+        .await
+        .unwrap();
+
+    let duration = tx.await.unwrap();
+
+    HttpResponse::Ok().body(format!(
+        "Node id: {node_id} found in {} ms\n",
+        duration.as_millis()
+    ))
+}
+
+async fn receiver_task(mut receiver: ForwardReceiver, pings: Pings) -> Result<()> {
+    while let Some(fwd) = receiver.recv().await {
+        log::info!("Handler got: {fwd:?}");
+
+        match fwd.transport {
+            ya_relay_client::TransportType::Reliable => {
+                let msg = String::from_utf8(fwd.payload.into_vec()).unwrap();
+                match msg.as_str() {
+                    "Ping" => {
+                        todo!()
+                    }
+                    "Pong" => {
+                        if let Some(sender) = pings.lock().unwrap().remove(1) {
+                            sender.send()
+                        }
+                    }
+                    _ => {}
+                }
+            }
+            ya_relay_client::TransportType::Unreliable => (),
+            ya_relay_client::TransportType::Transfer => (),
+        }
+    }
+
+    Ok(())
+}
+
+async fn client_task(client: Client, mut rx: mpsc::Receiver<Command>, pings: Pings) -> Result<()> {
     while let Some(cmd) = rx.recv().await {
         match cmd {
             Command::FindNode(FindNode { node_id, response }) => {
@@ -79,8 +144,28 @@ async fn client_handler(client: Client, mut rx: mpsc::Receiver<Command>) -> Resu
                         log::info!("Found node {node:?} in {} ms", elapsed.as_millis());
                         response.send(elapsed).unwrap();
                     }
-                    Err(e) => log::error!("{e}"),
+                    Err(e) => log::error!("Find node failed: {e}"),
                 }
+            }
+            Command::Ping(Ping { node_id, response }) => {
+                //TODO choose which transport type in API
+                match client.forward_reliable(node_id).await {
+                    Ok(mut sender) => {
+                        let msg = "Ping";
+                        if let Err(e) = sender.send(msg.as_bytes().to_vec().into()).await {
+                            log::error!("Send failed: {e}");
+                        } else {
+                            pings.lock().unwrap().insert(1, response);
+                        }
+                    }
+                    Err(e) => log::error!("Forward reliable failed: {e}"),
+                }
+
+                //Send ping to other node by forward sender and put response into hashmap
+
+                //forward receiver will
+                // If it got ping: response with pingresponse
+                // If got pingresponse then search in hashmap and send back duration
             }
         }
     }
@@ -95,14 +180,24 @@ async fn run() -> Result<()> {
     let (tx, rx) = mpsc::channel::<Command>(16);
     let client = build_client(cli.relay_addr, &cli.key_file, cli.password).await?;
 
+    let receiver = client.forward_receiver().await.unwrap();
+
+    let pings = Pings::default();
+    let pings_cloned = pings.clone();
+
     tokio::task::spawn_local(async move {
-        client_handler(client, rx).await.unwrap();
+        receiver_task(receiver, pings_cloned).await.unwrap();
+    });
+
+    tokio::task::spawn_local(async move {
+        client_task(client, rx, pings).await.unwrap();
     });
 
     HttpServer::new(move || {
         App::new()
             .app_data(Data::new(tx.clone()))
             .service(find_node)
+            .service(ping)
     })
     .workers(4)
     .bind(("0.0.0.0", cli.port))?
