2a92df6fe26f480cb42b85f0f26f57b2d99f1515
Staszek Krotki <16387248+staszek-krotki@users.noreply.github.com>
2023-07-27 10:03:57 +0200

    just one network

--- test_env/scenario1.sh
+++ test_env/scenario1.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-BLUE='\033[0;34m'
-# This script is used to test a network with two clients being on two separate networks without exposed ports
-
 # Start the network
 docker compose -f test_env/docker-compose-2nets.yml up \
     --detach \
@@ -16,11 +13,11 @@ docker compose -f test_env/docker-compose-2nets.yml up \
 #a nap
 sleep 5
 
-export EXPOSED_CONTAINER_ID=$(docker ps | grep exposed | awk '{print $1}')
-export HIDDEN_CONTAINER_ID=$(docker ps | grep hidden | awk '{print $1}')
+export EXPOSED_CONTAINER_ID=$(docker ps | grep exposed | awk '{print $1}'); echo $EXPOSED_CONTAINER_ID
+export HIDDEN_CONTAINER_ID=$(docker ps | grep hidden | awk '{print $1}'); echo $HIDDEN_CONTAINER_ID
 
-export EXPOSED_CLIENT_IP=$(docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $EXPOSED_CONTAINER_ID)
-export HIDDEN_CLIENT_IP=$(docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $HIDDEN_CONTAINER_ID)
+export EXPOSED_CLIENT_IP=$(docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $EXPOSED_CONTAINER_ID); echo $EXPOSED_CLIENT_IP
+export HIDDEN_CLIENT_IP=$(docker inspect -f '{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $HIDDEN_CONTAINER_ID); echo $HIDDEN_CLIENT_IP
 
 export EXPOSED_NODE_ID=$(docker logs $EXPOSED_CONTAINER_ID 2>&1 | grep "NODE ID" | awk '{print $6}'); echo $EXPOSED_NODE_ID
 export HIDDEN_NODE_ID=$(docker logs $HIDDEN_CONTAINER_ID 2>&1 | grep "NODE ID" | awk '{print $6}'); echo $HIDDEN_NODE_ID
@@ -45,11 +42,11 @@ export NETWORK_OF_HIDDEN_CLIENT=$(docker container inspect -f '{{range $net,$v :
 docker network disconnect $NETWORK_OF_HIDDEN_CLIENT $HIDDEN_CONTAINER_ID
 
 # echo -e "${Blue}Ping hidden client from exposed client\n"
-curl -X GET http://$EXPOSED_CLIENT_IP:8081/ping/$HIDDEN_NODE_ID
+curl --connect-timeout 5 -X GET http://$EXPOSED_CLIENT_IP:8081/ping/$HIDDEN_NODE_ID
 
 
 # echo "Find hidden client from exposed client"
-curl -X GET http://$EXPOSED_CLIENT_IP:8081/find-node/$HIDDEN_NODE_ID
+curl --connect-timeout 5 -X GET http://$EXPOSED_CLIENT_IP:8081/find-node/$HIDDEN_NODE_ID
 
 # echo "Find exposed client from hidden client"
 curl --connect-timeout 5 -X GET http://$HIDDEN_CLIENT_IP:8081/find-node/$EXPOSED_NODE_ID
