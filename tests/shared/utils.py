2b158ae9d4ed330052fa5fd8315eca945733a775
Przemyslaw Walski <pwalski@users.noreply.github.com>
2023-08-02 10:49:22 +0200

    Cluster WIP

--- tests/shared/utils.py
+++ tests/shared/utils.py
@@ -1,4 +1,6 @@
-from python_on_whales import docker
+from python_on_whales import docker, DockerClient, Container
+from dataclasses import dataclass
+from datetime import datetime, timedelta
 
 def set_netem(
         container,
@@ -11,3 +13,32 @@ def set_netem(
         command=command,
         privileged=True,
         )
+
+def __read_node_id(container: Container) -> str|None:
+    until = timedelta(seconds=10)
+    for log in docker.logs(container, stream=True, until=until):
+        if isinstance(log, str) and "CLIENT NODE ID: " in log:
+            node_id = log.split("CLIENT NODE ID: ")[1].strip()
+            return node_id
+    return None
+
+@dataclass
+class Node:
+    container: Container
+
+@dataclass
+class Server(Node):
+    pass
+
+@dataclass
+class Client(Node):
+    node_id: str
+
+# @dataclass
+class Cluster:
+    def from_client(compose_client: DockerClient) -> Cluster:
+        server = None
+        clients = []
+        for container in compose_client.container.list():
+            if "client" in container.name:
+                container.logs
