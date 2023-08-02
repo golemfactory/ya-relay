55ec11f10510f5b061e324bad58436288fb63b6b
Przemyslaw Walski <pwalski@users.noreply.github.com>
2023-08-03 01:00:57 +0200

    Calling ping

--- tests/shared/utils.py
+++ tests/shared/utils.py
@@ -1,6 +1,7 @@
 from python_on_whales import docker, DockerClient, Container
 from dataclasses import dataclass
 from datetime import datetime, timedelta
+from typing import Self
 import re
 
 def set_netem(
@@ -8,10 +9,9 @@ def set_netem(
         latency="0ms"
         ):
     print(f"Set netem for '{container.name}', latency {latency}")
-    command = ['tc', 'qdisc', 'replace', 'dev', 'eth0', 'root', 'netem', 'delay', latency]
     docker.execute(
         container=container,
-        command=command,
+        command=['tc', 'qdisc', 'replace', 'dev', 'eth0', 'root', 'netem', 'delay', latency],
         privileged=True,
         )
 
@@ -36,6 +36,15 @@ class Client(Node):
                 break
         assert re.match(r'0x[0-9a-fA-F]{40}', self.node_id), f"Invalid client Node Id {self.node_id} of container: {vars(container)}"
         print("New client. Node Id {}".format(self.node_id))
+
+    def ping(self, node: str|Self) -> str:
+        if isinstance(node, Self):
+            node = node.node_id
+        docker.execute(
+            container=self.container,
+            command=['curl', '-s', '-H' ,'Accept: application/json', f"localhost:8081/ping/{node}"],
+            )
+        # handle "Error response from daemon:"
 
 class Cluster:
     docker_client: DockerClient
