81c807ff31467b6424c38112d8ca0cbb55f24c32
Przemyslaw Walski <pwalski@users.noreply.github.com>
2023-09-05 13:00:45 +0200

    Util for waiting on events. Cleanup.

--- tests/test_net_down.py
+++ tests/test_net_down.py
@@ -4,6 +4,7 @@ from utils import set_netem, Cluster, Client, Server
 import time
 import pytest
 from threading import Thread, Event
+from typing import List
 import math
 
 LOGGER = logging.getLogger(__name__)
@@ -67,10 +68,10 @@ class PingJob(Thread):
 
 
 class Jobs(Thread):
-    jobs: [Thread]
+    jobs: List[Thread]
     jobs_started: Event
 
-    def __init__(self, ping_jobs: [Thread], ping_jobs_started: Event):
+    def __init__(self, ping_jobs: List[Thread], ping_jobs_started: Event):
         super(Jobs, self).__init__()
         self.jobs = ping_jobs
         self.jobs_started = ping_jobs_started
@@ -79,6 +80,14 @@ class Jobs(Thread):
         for job in self.jobs:
             job.start()
         self.jobs_started.set()
+
+
+def wait_for_events(events: List[Event], timeout: int):
+    start_time = time.time()
+    for event in events:
+        if start_time + timeout < time.time():
+            pytest.fail(f"Timeout")
+        event.wait(timeout=10)
 
 
 def test_net_down(compose_up):
@@ -113,10 +122,10 @@ def test_net_down(compose_up):
     except BaseException as error:
         assert f"{error}".index("Read timed out") >= 0
 
-    before_tr_events = []
-    after_tr_events = []
-    tr_jobs = []
-    for i in range(0, 3): 
+    before_tr_events: List[Event] = []
+    after_tr_events: List[Event] = []
+    tr_jobs: List[Thread] = []
+    for i in range(0, 3):
         before_tr = Event()
         after_tr = Event()
         data = bytearray(1_050_000)
@@ -125,11 +134,11 @@ def test_net_down(compose_up):
         before_tr_events.append(before_tr)
         after_tr_events.append(after_tr)
     tr_jobs_started = Event()
-    tr_jobs = Jobs(tr_jobs, tr_jobs_started)
+    tr_jobs_thread: Jobs = Jobs(tr_jobs, tr_jobs_started)
 
-    before_ping_events = []
-    after_ping_events = []
-    ping_jobs = []
+    before_ping_events: List[Event] = []
+    after_ping_events: List[Event] = []
+    ping_jobs: List[Thread] = []
     for i in range(0, 30):
         before_ping = Event()
         after_ping = Event()
@@ -138,16 +147,14 @@ def test_net_down(compose_up):
         before_ping_events.append(before_ping)
         after_ping_events.append(after_ping)
     ping_jobs_started = Event()
-    ping_jobs = Jobs(ping_jobs, ping_jobs_started)
+    ping_jobs_thread: Jobs = Jobs(ping_jobs, ping_jobs_started)
 
     LOGGER.info("Starting ping jobs and transfer job.")
-    ping_jobs.start()
-    tr_jobs.start()
+    ping_jobs_thread.start()
+    tr_jobs_thread.start()
 
-    for before_tr in before_tr_events:
-        before_tr.wait(timeout=10)  # It might last n*10s. Ignore it for now.
-    for before_ping in before_ping_events:
-        before_ping.wait(timeout=10)
+    wait_for_events(before_tr_events, 10)
+    wait_for_events(before_ping_events, 10)
 
     LOGGER.info("Sleepinng after jobs start.")
     time.sleep(3)
@@ -156,10 +163,8 @@ def test_net_down(compose_up):
     cluster.connect(client_hidden, networks)
 
     LOGGER.info("Connected. Waiting for Ping and Transfer jobs to finnish")
-    for after_tr in after_tr_events:
-        after_tr.wait(timeout=10)
-    for after_ping in after_ping_events:
-        after_ping.wait(timeout=10)
+    wait_for_events(after_tr_events, 10)
+    wait_for_events(after_ping_events, 10)
 
     # Consecutive pings should still work
     ping_response = client_exposed.ping(client_hidden.node_id)
