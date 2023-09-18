a9366fee10c736be2ec151b28190a500fa8805a3
Przemyslaw Walski <pwalski@users.noreply.github.com>
2023-09-18 16:14:59 +0200

    Session expiration test

--- tests_integration/test_priv_net.py
+++ tests_integration/test_priv_net.py
@@ -2,6 +2,7 @@ from python_on_whales import docker
 from utils import set_netem, Cluster, Client, Server
 import pytest
 import logging
+import time
 from typing import Set, List
 
 LOGGER = logging.getLogger(__name__)
@@ -47,3 +48,41 @@ def check_session_after_ping(
     sessions = client_1.sessions()
     sessions = {session["address"] for session in sessions["sessions"]}
     assert expected_sessions == sessions
+
+
+def test_session_expiration_after_disconnect(compose_up):
+    session_expiration = 3
+    cluster: Cluster = compose_up(
+        public_clients=2, alice_clients=1, bob_clients=1, build_args={"SESSION_EXPIRATION": session_expiration}
+    )
+
+    client_alice = cluster.clients("alice")[0]
+    client_bob = cluster.clients("bob")[0]
+    LOGGER.info("Testing session expiration on clients using relayed connection")
+    check_session_expiration_after_disconnect(cluster, client_alice, client_bob, session_expiration)
+
+    client_public_1 = cluster.clients("public")[0]
+    client_pubic_2 = cluster.clients("public")[1]
+    LOGGER.info("Testing session expiration on clients using p2p connection")
+    check_session_expiration_after_disconnect(cluster, client_public_1, client_pubic_2, session_expiration)
+
+
+def check_session_expiration_after_disconnect(
+    cluster: Cluster, client_1: Client, client_2: Client, session_expiration: int, ping_timeout: int = 13
+):
+    ping_response = client_1.ping(client_2.node_id)
+    assert client_2.node_id == ping_response["node_id"]
+
+    LOGGER.info("Disconnecting client")
+    disconnect_time = time.time()
+    cluster.disconnect(client_2)
+
+    try:
+        # Ping with reliable transport should fail on disconnected client which uses reliable transport.
+        # It shouuld happen after SESSION_EXPIRATION and before ping request timeout.
+        ping_response = client_1.ping(client_2.node_id, timeout=ping_timeout, transport="reliable")
+        pytest.fail(f"Ping to disconnected relayed client should fail. Instead got: {ping_response}")
+    except BaseException as error:
+        LOGGER.info(f"Ping error: {error}")
+        ping_error_delay = time.time() - disconnect_time
+        assert session_expiration < ping_error_delay and ping_timeout > ping_error_delay
