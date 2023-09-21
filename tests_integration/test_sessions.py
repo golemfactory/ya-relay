0fefb69ad1e4ab371eb3377a60341fb8ce118b37
Staszek Krotki <16387248+staszek-krotki@users.noreply.github.com>
2023-09-21 18:31:55 +0200

    combine tests

--- tests_integration/test_sessions.py
+++ tests_integration/test_sessions.py
@@ -51,7 +51,7 @@ def check_session_after_ping(
     assert expected_sessions == sessions
 
 
-def test_session_expiration_after_disconnect(compose_up):
+def test_session_expiration_after_disconnect_and_reinit_after_reconnect(compose_up):
     session_expiration = 3
     cluster: Cluster = compose_up(
         public_clients=2, alice_clients=1, bob_clients=1, build_args={"SESSION_EXPIRATION": session_expiration}
@@ -61,19 +61,27 @@ def test_session_expiration_after_disconnect(compose_up):
     LOGGER.info("Testing session expiration on clients using Relayed connection")
     client_0 = cluster.clients("alice")[0]
     client_1 = cluster.clients("bob")[0]
+    client_1_gateway = cluster.gateways("bob")[0]
     ping_response = client_0.ping(client_1.node_id)
     LOGGER.info("Check sessions after ping")
     assert client_1.node_id == ping_response["node_id"]
     check_sessions(client_0, {server.address()})
     check_sessions(client_1, {server.address()})
     LOGGER.info("Disconnecting bob")
-    cluster.disconnect(client_1)
+    client_1_networks = cluster.disconnect(client_1)
     time.sleep(1)
     LOGGER.info("Check sessions right after disconnect")
     check_sessions(client_0, {server.address()})
     time.sleep(5)
     LOGGER.info("Check sessions after session expiration period")
     check_sessions(client_0, {server.address()})
+
+    LOGGER.info("Check server session was lost after disconnect but gets reconnected")
+    cluster.connect(client_1, client_1_networks)
+    client_1.reset_gateway(client_1_gateway.address("bob"))
+    check_sessions(client_1, {})
+    time.sleep(5)
+    check_sessions(client_1, {server.address()})
 
     LOGGER.info("Testing session expiration on clients using P2P connection")
     client_0 = cluster.clients("public")[0]
@@ -85,35 +93,20 @@ def test_session_expiration_after_disconnect(compose_up):
     check_sessions(client_1, {server.address(), client_0.address()})
     LOGGER.info("Disconnecting p2p client")
     client_1_address = client_1.address()
-    cluster.disconnect(client_1)
+    client_1_networks = cluster.disconnect(client_1)
     time.sleep(1)
     LOGGER.info("Check sessions right after disconnect")
     check_sessions(client_0, {server.address(), client_1_address})
     time.sleep(5)
     LOGGER.info("Check sessions after session expiration period")
+    LOGGER.info(f"address: {client_0.address()} {client_1.address()} {server.address()}")
     check_sessions(client_0, {server.address()})
 
-
-def test_server_session_restored_after_disconnect(compose_up):
-    session_expiration = 3
-    cluster: Cluster = compose_up(
-        public_clients=1, alice_clients=0, bob_clients=0, build_args={"SESSION_EXPIRATION": session_expiration}
-    )
-    server: Server = cluster.servers()[0]
-
-    LOGGER.info("Testing server session after start")
-    client_0 = cluster.clients("public")[0]
-    check_sessions(client_0, {server.address()})
-
-    LOGGER.info("Testing server session is lost after disconnect")
-    networks_0 = cluster.disconnect(client_0)
+    LOGGER.info("Check server session was lost after disconnect but gets reconnected")
+    cluster.connect(client_1, client_1_networks)
+    check_sessions(client_1, {client_0.address()})
     time.sleep(5)
-    cluster.connect(client_0, networks_0)
-    check_sessions(client_0, {})
-
-    LOGGER.info("Testing server session is restored")
-    time.sleep(5)
-    check_sessions(client_0, {server.address()})
+    check_sessions(client_1, {client_0.address(), server.address()})
 
 
 def test_ping_after_disconnect_and_reconnect(compose_up):
@@ -135,15 +128,10 @@ def test_ping_after_disconnect_and_reconnect(compose_up):
         alice.ping(bob.node_id)
     assert "timeout" in str(excinfo.value)
 
-    LOGGER.info("Testing sessions after disconnect")
-    time.sleep(5)
+    LOGGER.info("Testing ping after reconnect")
     cluster.connect(bob, networks)
     bob.reset_gateway(bob_gateway.address("bob"))
-    check_sessions(bob, {})
-
-    LOGGER.info("Testing ping after reconnect")
     time.sleep(1)
-    check_sessions(bob, {server.address()})
 
     try:
         alice.ping(bob.node_id)
@@ -153,6 +141,5 @@ def test_ping_after_disconnect_and_reconnect(compose_up):
 
 def check_sessions(client: Client, expected_sessions: Set[Any] | Dict[Any, Any]):
     client_sessions = client.sessions()
-    LOGGER.info(f"Sessions: {client_sessions}")
     actual_sessions = {session["address"] for session in client_sessions["sessions"]}
     assert len(expected_sessions) == 0 and len(actual_sessions) == 0 or expected_sessions == actual_sessions
