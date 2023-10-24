90f0dc359d1b560737861c9119934b93e9a0b120
Attila Mravik <4715833+evik42@users.noreply.github.com>
2023-10-24 13:06:52 +0200

    Simultaneous session initialization test

--- tests_integration/test_sessions.py
+++ tests_integration/test_sessions.py
@@ -1,5 +1,6 @@
 from python_on_whales import docker
-from utils import set_netem, Cluster, Client, Server
+from utils import Cluster, Client, Server
+import concurrent.futures
 import pytest
 import logging
 import time
@@ -7,6 +8,26 @@ import requests
 from typing import Set, Any, Dict
 
 LOGGER = logging.getLogger(__name__)
+
+
+def check_session_after_ping(
+    client_1: Client,
+    client_2: Client,
+    expected_sessions: Set[str],
+):
+    client_1.ping(client_2.node_id)
+
+    check_sessions(client_1, expected_sessions)
+
+
+def check_sessions(client: Client, expected_sessions: Set[Any] | Dict[Any, Any]):
+    client_sessions = client.sessions()
+    actual_sessions = {session["address"] for session in client_sessions["sessions"]}
+    try:
+        assert len(expected_sessions) == 0 and len(actual_sessions) == 0 or expected_sessions == actual_sessions
+    except AssertionError as e:
+        LOGGER.info(f"Client logs for {client.container.name}\n{client.logs()}")
+        raise e
 
 
 def test_sessions_with_and_without_p2p(compose_up):
@@ -39,16 +60,6 @@ def test_sessions_with_and_without_p2p(compose_up):
     expected_sessions = {server.address()}
     LOGGER.info(f"Testing session between 'alice' and 'bob' network")
     check_session_after_ping(client_1, client_2, expected_sessions)
-
-
-def check_session_after_ping(
-    client_1: Client,
-    client_2: Client,
-    expected_sessions: Set[str],
-):
-    client_1.ping(client_2.node_id)
-
-    check_sessions(client_1, expected_sessions)
 
 
 def test_session_expiration_after_disconnect_and_reinit_after_reconnect(compose_up):
@@ -146,11 +157,28 @@ def test_ping_after_disconnect_and_reconnect(compose_up):
         pytest.fail(f"Unexpected exception raised: {excinfo}")
 
 
-def check_sessions(client: Client, expected_sessions: Set[Any] | Dict[Any, Any]):
-    client_sessions = client.sessions()
-    actual_sessions = {session["address"] for session in client_sessions["sessions"]}
-    try:
-        assert len(expected_sessions) == 0 and len(actual_sessions) == 0 or expected_sessions == actual_sessions
-    except AssertionError as e:
-        LOGGER.info(f"Client logs for {client.container.name}\n{client.logs()}")
-        raise e
+# For now, simultaneous session initialization prevents p2p connection as both nodes think they initiated
+# the session and drop the incoming initialization packet with a log line:
+# [DEBUG ya_relay_client::session::network_view] Initialization of session with: [0xXX] in progress, state: Outgoing-Initializing. Thread will wait for finish.
+# When this fixed, the test should show a direct session between the two nodes, instead of the relayed connection.
+def test_simultaneous_session_initialization(compose_up):
+    cluster: Cluster = compose_up(
+        public_clients=2,
+        alice_clients=0,
+        bob_clients=0,
+        build_args={"CLIENT_LATENCY": "300ms", "RUST_LOG": "info,ya_relay_client::session=trace"},
+    )
+    server: Server = cluster.servers()[0]
+
+    LOGGER.info("Testing simultaneous session initialization")
+    client_0 = cluster.clients("public")[0]
+    client_1 = cluster.clients("public")[1]
+
+    with concurrent.futures.ThreadPoolExecutor(max_workers=2) as executor:
+        executor.submit(client_0.ping, client_1.node_id)
+        executor.submit(client_1.ping, client_0.node_id)
+
+    check_sessions(client_0, {server.address()})
+    # LOGGER.info(f"Client logs for {client_0.container.name}\n{client_0.logs()}")
+    check_sessions(client_1, {server.address()})
+    # LOGGER.info(f"Client logs for {client_1.container.name}\n{client_1.logs()}")
